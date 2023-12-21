04fc7ec9029b13bcf7574ccba7899c645a966d52
Kevin Uruchi <uruchicoarite@gmail.com>
2023-12-21 14:32:23 -0400

    envs added and logs working

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,16 +1,19 @@
+import { LogEntity, LogSeverityLevel } from "../../entities/log.entity";
+import { LogRepository } from "../../repository/log.repository";
 
 interface CheckServiceUseCase {
     execute( url: string ):Promise<boolean>;
 }
 
 //inyeccion de dependencias
-type SuccessCallback = () => void;
-type ErrorCallback = ( error: string ) => void;
+type SuccessCallback = (() => void) | undefined;
+type ErrorCallback = (( error: string ) => void) | undefined;
 
 
 export class CheckService {
 
     constructor(
+        private readonly logRepository: LogRepository,
         private readonly successCallback: SuccessCallback,
         private readonly errorCallback: ErrorCallback,
     ) {}
@@ -24,14 +27,18 @@ export class CheckService {
                 throw new Error(`Error on check service ${ url }`);
             }
 
-            this.successCallback();
+            const log = new LogEntity(`Service ${ url } working`, LogSeverityLevel.low);
+            this.logRepository.saveLog( log );
+            this.successCallback && this.successCallback();
             // console.log(`${url} is ok`);
             return true;
         } catch (error) {
 
-            console.log(`${error}`);
-
-            this.errorCallback(`${error}`);
+            // console.log(`${error}`);
+            const errorMessage = `${url} is not ok. ${ error }`;
+            const log = new LogEntity( errorMessage, LogSeverityLevel.high);
+            this.logRepository.saveLog( log );
+            this.errorCallback && this.errorCallback( errorMessage );
             return false;
         }
 
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,12 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log,repository.impl";
 import { CronService } from "./cron/cron-service";
 
 
+const fileSystemLogRepository = new LogRepositoryImpl(
+    new FileSystemDatasource(),
+);
 
 export class Server {
 
@@ -12,11 +17,12 @@ export class Server {
         CronService.createJob(
             '*/5 * * * * *',
             () => {
-                const url = 'https://google.com'
+                const url = 'http://localhost:3000'
                 // const date = new Date();
                 // console.log('5 second', date);
 
                 new CheckService(
+                    fileSystemLogRepository,
                     //inyeccion de dependencias
                     () => console.log( `${url} is ok`),
                     ( error ) => console.log( error),
