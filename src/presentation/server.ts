7fe9f1bccca5bdb4331cf8e3fd153557e80e842c
Kevin Uruchi <uruchicoarite@gmail.com>
2023-12-22 01:31:05 -0400

    added email sending

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,12 +1,14 @@
-import { CheckService } from "../domain/use-cases/checks/check-service";
+import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log,repository.impl";
-import { CronService } from "./cron/cron-service";
+import { EmailService } from "./email/email.service";
 
 
 const fileSystemLogRepository = new LogRepositoryImpl(
     new FileSystemDatasource(),
 );
+
+const emailService = new EmailService();
 
 export class Server {
 
@@ -14,23 +16,42 @@ export class Server {
 
         console.log('Server started...');
 
-        CronService.createJob(
-            '*/5 * * * * *',
-            () => {
-                const url = 'http://localhost:3000'
-                // const date = new Date();
-                // console.log('5 second', date);
+        //todo: Mandar Email
+        // new SendEmailLogs(
+        //     emailService,
+        //     fileSystemLogRepository,
+        // ).execute('uruchicoarite@outlook.com')
+        // emailService.sendEmail({
+        //     to: 'uruchicoarite@outlook.com',
+        //     subject: 'Logs del sistema',
+        //     htmlBody: `
+        //         <h3>Logs del sistema - NOC</h3>
+        //         <p>Lorenajksjbasbdbasdkjansdnas</p>
+        //         <p>Lorenajksjbasbdbasdkjansdnas</p>
+        //     `
+        // })
 
-                new CheckService(
-                    fileSystemLogRepository,
-                    //inyeccion de dependencias
-                    () => console.log( `${url} is ok`),
-                    ( error ) => console.log( error),
-                ).execute( url );
-                // new CheckService().execute( 'http://localhost:3000' );
+        // emailService.sendEmailWithFileSystemLogs(
+        //     'uruchicoarite@outlook.com',
+        // );
 
-            }
-        );
+        // CronService.createJob(
+        //     '*/5 * * * * *',
+        //     () => {
+        //         const url = 'https://google.com'
+        //         // const date = new Date();
+        //         // console.log('5 second', date);
+
+        //         new CheckService(
+        //             fileSystemLogRepository,
+        //             //inyeccion de dependencias
+        //             () => console.log( `${url} is ok`),
+        //             ( error ) => console.log( error),
+        //         ).execute( url );
+        //         // new CheckService().execute( 'http://localhost:3000' );
+
+        //     }
+        // );
 
     }
 
